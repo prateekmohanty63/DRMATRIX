1008ddb1d510cff01a2e39ae78e8d9f419d75b2c
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-05-18 22:18:01 +0530

    Feat(Note fetching) Completed not fetching using useState and useEffect

--- noteatom/src/pages/NotePage.js
+++ noteatom/src/pages/NotePage.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import notes from "../assets/data";
+//import notes from "../assets/data";
 import { Link } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
 
 const NotePage = (props) => {
   // console.log("props: ", props);
   const { id } = useParams();
-  let note = notes.find((note) => note.id === Number(id));
+  //let note = notes.find((note) => note.id === Number(id));
+  let [note, setNote] = useState(null); // always initilize state at the top (good practice)
+
+  useEffect(() => {
+    getNote();
+  }, [id]);
+
+  let getNote = async () => {
+    let response = await fetch(`http://localhost:8000/notes/${id}`);
+    let data = await response.json();
+    console.log(data);
+    setNote(data);
+  };
 
   console.log(id);
   return (
--- noteatom/src/pages/NotesListPage.js
+++ noteatom/src/pages/NotesListPage.js
@@ -1,8 +1,22 @@
-import React from "react";
-import notes from "../assets/data";
+import React, { useEffect, useState } from "react";
+//import notes from "../assets/data";
 import { ListItem } from "../components/ListItem";
 
 const NotesListPage = () => {
+  let [notes, setNotes] = useState([]);
+
+  // use effect to change state of the notes
+
+  useEffect(() => {
+    getNotes();
+  }, []); // [depenciences] the useEffect will be called on every change
+
+  let getNotes = async () => {
+    let response = await fetch("http://localhost:8000/notes/");
+    let data = await response.json();
+    setNotes(data);
+  };
+
   return (
     <div className="notes">
       <div className="notes-header">
