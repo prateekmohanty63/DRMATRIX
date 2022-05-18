089cc242bcafebc773926437e635589f4142f80a
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-05-19 00:27:10 +0530

    Completed Note Atom -- onto deployment

--- noteatom/src/components/ListItem.js
+++ noteatom/src/components/ListItem.js
@@ -1,11 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+let getDate = (note) => {
+  return new Date(note.updated).toLocaleDateString();
+};
+
+let getTitle = (note) => {
+  const title = note.body.split("\n")[0];
+
+  if (title.length > 45) {
+    return title.slice(0, 45);
+  }
+
+  return title;
+};
+
+let getContent = (note) => {
+  let title = getTitle(note);
+  let content = note.body.replaceAll("\n", " ");
+  content = content.replaceAll(title, "");
+
+  if (content.length > 45) {
+    return content.slice(0, 45);
+  } else {
+    return content;
+  }
+};
+
 export const ListItem = ({ note }) => {
   return (
     <Link to={`/note/${note.id}`}>
       <div className="notes-list-item">
-        <h3>{note.body}</h3>
+        <h3>{getTitle(note)}</h3>
+        <p>
+          <span>{getDate(note)}</span>
+          {getContent(note)}
+        </p>
       </div>
     </Link>
   );
