a5948cf3d127d54fb36593bd88ff129caac09911
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-05-18 23:03:11 +0530

    Feat(Udpate data) Update data completed

--- noteatom/src/pages/NotePage.js
+++ noteatom/src/pages/NotePage.js
@@ -3,10 +3,12 @@ import { useParams } from "react-router-dom";
 //import notes from "../assets/data";
 import { Link } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
+import { useNavigate } from "react-router-dom";
 
 const NotePage = (props) => {
   // console.log("props: ", props);
   const { id } = useParams();
+  let navigate = useNavigate();
   //let note = notes.find((note) => note.id === Number(id));
   let [note, setNote] = useState(null); // always initilize state at the top (good practice)
 
@@ -21,20 +23,40 @@ const NotePage = (props) => {
     setNote(data);
   };
 
+  let updateNote = async () => {
+    await fetch(`http://localhost:8000/notes/${id}`, {
+      method: "PUT",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...note, updated: new Date() }),
+    });
+  };
+
+  let handleSubmit = () => {
+    updateNote();
+    navigate("/");
+  };
+
   console.log(id);
   return (
     <div className="note">
       <div className="note-header">
         <h3>
           <Link to="/">
-            <ArrowLeft />
+            <ArrowLeft onClick={handleSubmit} />
           </Link>
         </h3>
       </div>
 
       {/* <div>{note.body}</div> */}
 
-      <textarea value={note?.body}></textarea>
+      <textarea
+        onChange={(e) => {
+          setNote({ ...note, body: e.target.value });
+        }}
+        value={note?.body}
+      ></textarea>
     </div>
   );
 };
